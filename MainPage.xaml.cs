fc920cb70e0655ea08d30d2ef4d5600ae7d169e9
Victor souza <luanvitorlima2017@gmail.com>
2025-08-24 11:20:33 -0300

    gerando numeros aleatorios do sorteio

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace CursoMaui;
 
 public partial class MainPage : ContentPage
@@ -6,17 +8,35 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void OnClickButton(object sender, EventArgs e)
+    {
+        OnGenerateDialogBox();
+    }
 
-	private void OnGenerateLuckNumber()
+    private void OnGenerateDialogBox()
 	{
 		LuckAppTitle.IsVisible = false;
 		ContainerLuckNumber.IsVisible = true;
+		var set = OnClickGenerateLuckNumber();
+		LuckNumber01.Text = set.ElementAt(0).ToString("D2", CultureInfo.InvariantCulture);
+		LuckNumber02.Text = set.ElementAt(1).ToString("D2", CultureInfo.InvariantCulture);
+		LuckNumber03.Text = set.ElementAt(2).ToString("D2", CultureInfo.InvariantCulture);
+		LuckNumber04.Text = set.ElementAt(3).ToString("D2", CultureInfo.InvariantCulture);
+		LuckNumber05.Text = set.ElementAt(4).ToString("D2", CultureInfo.InvariantCulture);
+		LuckNumber06.Text = set.ElementAt(5).ToString("D2", CultureInfo.InvariantCulture);	
     }
 
-	private void OnClickButton(object sender, EventArgs e)
+	private SortedSet<int> OnClickGenerateLuckNumber()
 	{
-		OnGenerateLuckNumber();
+		var set = new SortedSet<int>();
+
+		while (set.Count < 6)
+        {
+            Random random = new Random();
+            int luckNumber = random.Next(1, 60);
+            set.Add(luckNumber);
+        }
+		
+		return set;
     }
-
-
 }
